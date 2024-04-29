5461210a50fd306654e44a0bc4b747fef5718aa1
MikolajMroczkowski <mikolaj.mroczkowski@gmail.com>
2024-04-30 00:14:35 +0200

    fixed linux version

--- app/src/main/spleeter/PythonEnvironment.js
+++ app/src/main/spleeter/PythonEnvironment.js
@@ -1,51 +1,49 @@
-const { execSync,exec } = require('child_process');
+const {execSync, exec} = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
 class PythonEnvironment {
-    constructor(venvPath) {
-        this.venvPath = venvPath;
-        console.log(__dirname);
-        //this.createEnvironment = this.createEnvironment.bind(this);
-    }
+  constructor(venvPath) {
+    this.venvPath = venvPath;
+    console.log(__dirname);
+    //this.createEnvironment = this.createEnvironment.bind(this);
+  }
 
-    createEnvironment() {
-        if (!fs.existsSync(this.venvPath)) {
-            const pythonExecutable = "python3.7"
-            const createCommand = `${pythonExecutable} -m venv ${this.venvPath}`;
-            try {
-                execSync(createCommand);
-                return {status: true, message: 'Environment created successfully'};
-            }
-            catch (e) {
-                return {status: false, message: e.message};
-            }
-        } else {
-            return {status: true, message: 'Environment already exists'};
+  createEnvironment() {
+    if (!fs.existsSync(this.venvPath)) {
+      const pythonExecutable = "python3.7"
+      const createCommand = `${pythonExecutable} -m venv ${this.venvPath}`;
+      try {
+        execSync(createCommand);
+        return {status: true, message: 'Environment created successfully'};
+      } catch (e) {
+        return {status: false, message: e.message+"\nmake sure you installed python3.7 with venv support"};
+      }
+    } else {
+      return {status: true, message: 'Environment already exists'};
+    }
+  }
+
+  runCommand(command) {
+    const activateScript = this.isWindows() ? 'Scripts\\activate.bat' : 'bin/activate';
+    const activatePath =this.isWindows()? path.join(this.venvPath, activateScript): `. ./`+path.join(this.venvPath, activateScript);
+    const activateCommand = this.isWindows() ? activatePath : `${activatePath}`;
+    const fullCommand = `${activateCommand} && ${command}`;
+    //const result = execSync(fullCommand).toString();
+    return new Promise((resolve, reject) => {
+      exec(fullCommand, (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+          return;
         }
-    }
+        resolve(stdout);
+      });
+    })
+  }
 
-    runCommand(command) {
-        const activateScript = this.isWindows() ? 'Scripts\\activate.bat' : 'bin/activate';
-        const activatePath = path.join(this.venvPath, activateScript);
-        const activateCommand = this.isWindows() ? activatePath : `source ${activatePath}`;
-
-        const fullCommand = `${activateCommand} && ${command}`;
-        //const result = execSync(fullCommand).toString();
-        return new Promise((resolve, reject) => {
-            exec(fullCommand, (error, stdout, stderr) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-                resolve(stdout);
-            });
-        })
-    }
-
-    isWindows() {
-        return process.platform === 'win32';
-    }
+  isWindows() {
+    return process.platform === 'win32';
+  }
 }
 
 export default PythonEnvironment;
--- app/src/main/spleeter/spleeter.js
+++ app/src/main/spleeter/spleeter.js
@@ -1,6 +1,7 @@
 import PythonEnvironment from "./PythonEnvironment";
 import * as path from "node:path";
 import {app} from "electron";
+
 const {existsSync} = require("node:fs");
 const {copyFileSync} = require("fs");
 const ffmpeg = require("ffmpeg-ffprobe-static");
@@ -12,7 +13,7 @@ const prepare = async () => {
     console.log(envWorking)
     if (!envWorking.status) {
       console.log('Error creating environment')
-      return {status: false, message:envWorking.message };
+      return {status: false, message: envWorking.message};
     }
 
     const filePath = path.resolve(app.getAppPath(), 'resources', 'pythonTest.py');
@@ -35,46 +36,43 @@ const prepare = async () => {
   } catch (error) {
     console.error('Wystąpił błąd podczas przygotowywania środowiska:', error);
     let errForUser
-    if(error.message.includes(" Failed to establish a new connection")){
-      errForUser="Internet error!\nProbably no internet connection\nEnv can't be downloaded"
+    if (error.message.includes(" Failed to establish a new connection")) {
+      errForUser = "Internet error!\nProbably no internet connection\nEnv can't be downloaded"
+    } else {
+      errForUser = error.message
     }
-    else{
-      errForUser=error.message
-    }
-    return {status: false, message:errForUser};
+    return {status: false, message: errForUser};
   }
 }
 
-const checkEnv = ()=>{
-    if(existsSync('dse')) {
-        const py = new PythonEnvironment('dse')
-        const binDir = py.isWindows()?'Scripts':'bin'
-        const spleeterDir = `dse/${binDir}/${py.isWindows()? 'spleeter.exe':'spleeter'}`
-        const ffmpegDir = py.isWindows()? 'ffmpeg.exe':'ffmpeg'
-        if(existsSync(spleeterDir)) {
-            console.log('spleeter is installed')
-            if(existsSync(ffmpegDir)) {
-                console.log('ffmpeg is installed')
-                return {status: true, message: 'spleeter and ffmpeg are installed'}
-            }
-            else{
-                console.log('ffmpeg is not installed')
-                return {status: false, message: 'ffmpeg is not installed'}
-            }
-        }
-        else{
-            return {status: false, message: 'spleeter is not installed'}
-        }
+const checkEnv = () => {
+  if (existsSync('dse')) {
+    const py = new PythonEnvironment('dse')
+    const binDir = py.isWindows() ? 'Scripts' : 'bin'
+    const spleeterDir = `dse/${binDir}/${py.isWindows() ? 'spleeter.exe' : 'spleeter'}`
+    const ffmpegDir = py.isWindows() ? 'ffmpeg.exe' : 'ffmpeg'
+    if (existsSync(spleeterDir)) {
+      console.log('spleeter is installed')
+      if (existsSync(ffmpegDir)) {
+        console.log('ffmpeg is installed')
+        return {status: true, message: 'spleeter and ffmpeg are installed'}
+      } else {
+        console.log('ffmpeg is not installed')
+        return {status: false, message: 'ffmpeg is not installed'}
+      }
+    } else {
+      return {status: false, message: 'spleeter is not installed'}
     }
-    return {status: false, message: 'env does not exist'}
+  }
+  return {status: false, message: 'env does not exist'}
 
 }
-const run=(input,out,model)=>{
-    const py = new PythonEnvironment('dse')
-    const command = `spleeter separate -o "${out}" -p ${model} "${input}"`
-    console.log(command)
-    const res = py.runCommand(command)
-    console.log(res)
-    return res
+const run = (input, out, model) => {
+  const py = new PythonEnvironment('dse')
+  const command = `spleeter separate -o "${out}" -p ${model} "${input}"`
+  console.log(command)
+  const res = py.runCommand(command)
+  console.log(res)
+  return res
 }
-export {prepare,checkEnv,run}
+export {prepare, checkEnv, run}
